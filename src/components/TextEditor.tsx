91a6ed2f3257fc507a004dc76096dcda0a534ff2
sarin <sarinmsari@gmail.com>
2026-02-09 01:57:46 +0530

    Update dependencies and enhance TextEditor component with new features. Added @emotion and @mui libraries for styling and icons, integrated underline functionality in the text editor, and improved the bubble menu with additional formatting options.

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -3,12 +3,24 @@ import { useEditor, EditorContent } from '@tiptap/react'
 import { FloatingMenu, BubbleMenu } from '@tiptap/react/menus'
 import StarterKit from '@tiptap/starter-kit'
 import Placeholder from '@tiptap/extension-placeholder'
+import Underline from '@tiptap/extension-underline'
 import './../assets/styles/text_editor.css'
+
+import BoldIcon from '@mui/icons-material/FormatBold';
+import ItalicIcon from '@mui/icons-material/FormatItalic';
+import UnderlineIcon from '@mui/icons-material/FormatUnderlined';
+import StrikethroughIcon from '@mui/icons-material/StrikethroughS';
+import CodeIcon from '@mui/icons-material/Code';
+import QuoteIcon from '@mui/icons-material/FormatQuote';
+import ListIcon from '@mui/icons-material/FormatListBulleted';
+import NumberedListIcon from '@mui/icons-material/FormatListNumbered';
+import { Icon, IconButton } from '@mui/material';
 
 const TextEditor = () => {
   const editor = useEditor({
     extensions: [
         StarterKit,
+        Underline,
         Placeholder.configure({
             placeholder: 'Start typing here or press “/” for commands',
             // This class is added to the editor when it's empty
@@ -16,13 +28,38 @@ const TextEditor = () => {
         }),
     ], // define your extension array
     content: '', // initial content
+    onUpdate: () => {},
+    onSelectionUpdate: () => {},
   })
+
+  if (!editor) {
+    return null
+  }
 
   return (
     <>
       <EditorContent editor={editor} />
-      <FloatingMenu editor={editor}>This is the floating menu</FloatingMenu>
-      <BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu>
+      {/* <FloatingMenu editor={editor}>This is the floating menu</FloatingMenu> */}
+      <BubbleMenu editor={editor} className='bg-mainBg px-4 py-2 rounded-xl border border-border'>
+            <IconButton onClick={() => editor.chain().focus().toggleBold().run()}>
+                <BoldIcon className={`${editor.isActive('bold') ? 'text-textPrimary' : 'text-textSecondary'} hover:text-textPrimary`} />
+            </IconButton>
+            <IconButton onClick={() => editor.chain().focus().toggleItalic().run()}>
+                <ItalicIcon className={`${editor.isActive('italic') ? 'text-textPrimary' : 'text-textSecondary'} hover:text-textPrimary`} />
+            </IconButton>
+            <IconButton onClick={() => editor.chain().focus().toggleUnderline().run()}>
+                <UnderlineIcon className={`${editor.isActive('underline') ? 'text-textPrimary' : 'text-textSecondary'} hover:text-textPrimary`} />
+            </IconButton>
+            {/* <IconButton onClick={() => editor.chain().focus().toggleStrikethrough().run()}>
+                <StrikethroughIcon className="text-textSecondary hover:text-textPrimary" />
+            </IconButton> */}
+            <IconButton onClick={() => editor.chain().focus().toggleCode().run()}>
+                <CodeIcon className={`${editor.isActive('code') ? 'text-textPrimary' : 'text-textSecondary'} hover:text-textPrimary`} />
+            </IconButton>
+            {/* <IconButton onClick={() => editor.chain().focus().toggleQuote().run()}>
+                <QuoteIcon className="text-textSecondary hover:text-textPrimary" />
+            </IconButton> */}
+      </BubbleMenu>
     </>
   )
 }
